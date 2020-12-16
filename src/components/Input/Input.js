a1061677df18c34f122fd4b94bb52b512b2e25c8
dorkele <dora_tominic@hotmail.com>
2020-12-16 22:15:34 +0100

    Add dispatch on enter

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,30 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import styles from "./Input.module.css";
 import Button from "../Button/Button";
 import addMessage from "../../redux/actions";
 import { useDispatch } from "react-redux";
 
 export default function Input() {
+    const elemRef = useRef();
     const [inputValue, setInputValue] = useState("");
-    console.log(inputValue);
+
     const dispatch = useDispatch();
 
     const handleInput = (e) => {
         setInputValue(e.target.value);
     };
-    const handleClick = () => {
-        console.log("i was clicked");
-        if (!inputValue) {
-            return;
-        } else {
-            dispatch(addMessage(inputValue));
+
+    const keyCheck = (e) => {
+        if (e.key === "Enter") {
+            handleClick();
         }
     };
 
+    const handleClick = () => {
+        inputValue && dispatch(addMessage(inputValue));
+        elemRef.current.value = "";
+    };
+
     return (
-        <div className={styles.msg_container}>
-            <input type="text" placeholder="message" onChange={handleInput} />
-            <Button buttonText="Send" onClick={handleClick()} />
+        <div className={styles.fixed_container}>
+            <div className={styles.input_container}>
+                <input
+                    className={styles.input}
+                    type="text"
+                    placeholder="message"
+                    onChange={handleInput}
+                    ref={elemRef}
+                    onKeyPress={(e) => {
+                        keyCheck(e);
+                    }}
+                />
+                <Button buttonText="Send" onClick={handleClick} />
+            </div>
         </div>
     );
 }
