abeca28917116cf685f9aafef7dea6937c2f7e44
dorkele <dora_tominic@hotmail.com>
2020-12-16 22:37:40 +0100

    Add setInterval to check for other user's messages

--- src/components/MsgList/MsgList.js
+++ src/components/MsgList/MsgList.js
@@ -1,35 +1,58 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import styles from "./MsgList.module.css";
 import { useSelector, useDispatch } from "react-redux";
 import { getChatMessages } from "../../redux/actions";
 
 export default function MsgList() {
     const dispatch = useDispatch();
-    const chatMessages = useSelector(
-        (state) => state.messages && state.messages
-    );
+    const elemRef = useRef();
+    const chatMessages = useSelector((state) => state && state.messages);
+
     useEffect(() => {
         dispatch(getChatMessages());
-    }, []);
+    }, [dispatch]);
 
-    console.log("chatMsgs: ", chatMessages);
+    useEffect(() => {
+        const timer = setInterval(function () {
+            dispatch(getChatMessages());
+        }, 1000);
+        return () => {
+            clearInterval(timer);
+        };
+    }, [dispatch]);
+
+    useEffect(() => {
+        if (chatMessages) {
+            elemRef.current.scrollTop =
+                elemRef.current.scrollHeight - elemRef.current.clientHeight;
+        }
+    }, [chatMessages]);
+
     return (
-        <React.Fragment>
-            {chatMessages &&
-                chatMessages.map((message) => {
-                    return (
-                        <div
-                            key={message._id}
-                            className={styles.comment_container}
-                        >
-                            <div>{message.author}</div>
-                            <div>{message.message}</div>
-                            <div className="black-font name-date">
-                                {new Date(message.timestamp).toLocaleString()}
+        <div ref={elemRef} className={styles.container}>
+            <div className={styles.comments_container}>
+                {chatMessages &&
+                    chatMessages.map((message) => {
+                        return (
+                            <div
+                                key={message.id}
+                                className={styles.comment_container}
+                            >
+                                <div className={styles.info}>
+                                    {message.author}
+                                </div>
+                                <div className={styles.message}>
+                                    {message.message}
+                                </div>
+                                <div className={styles.info}>
+                                    {new Date(
+                                        message.timestamp
+                                    ).toLocaleString()}
+                                </div>
                             </div>
-                        </div>
-                    );
-                })}
-        </React.Fragment>
+                        );
+                    })}
+            </div>
+        </div>
     );
 }
