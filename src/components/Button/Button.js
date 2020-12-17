68dbe8fd9afb13c2916fc7a8c463819a0a28e7b7
dorkele <dora_tominic@hotmail.com>
2020-12-17 20:10:34 +0100

    Add safeguard for onClick prop

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,8 +2,15 @@ import React from "react";
 import styles from "./Button.module.css";
 
 export default function Button({ buttonText, onClick }) {
+    //check that a function is passed to Button component
+    const handleClick = () => {
+        if (typeof onClick === "function") {
+            onClick();
+        }
+    };
+
     return (
-        <button onClick={onClick} className={styles.button}>
+        <button onClick={handleClick} className={styles.button}>
             {buttonText}
         </button>
     );
