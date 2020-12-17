111716878f0b8bafb48cc96d6e83dfa1933e89e7
dorkele <dora_tominic@hotmail.com>
2020-12-17 19:19:03 +0100

    Refactor to get nickname from state and show NicknameModal component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import styles from "./App.module.css";
-import Input from "../Input/Input";
+import MessageInput from "../MessageInput/MessageInput";
 import MsgList from "../MsgList/MsgList";
+import NicknameModal from "../NicknameModal/NicknameModal";
+import { useSelector, useDispatch } from "react-redux";
+import { getUser } from "../../redux/actions";
 
 export default function App() {
+    const dispatch = useDispatch();
+
+    //get user's nickname from state
+    useEffect(() => {
+        dispatch(getUser());
+    }, [dispatch]);
+
+    const nickname = useSelector((state) => state && state.user);
+
     return (
         <div className={styles.app_container}>
+            {!nickname && <NicknameModal />}
             <MsgList />
-            <Input />
+            <MessageInput user={nickname} />
         </div>
     );
 }
